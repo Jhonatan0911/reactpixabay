79975ea3accd55f06fcb430aeb274b4970efbf90
Jhonatan0911 <jpinalozano911@gmail.com>
2022-02-23 10:19:33 -0500

    Usando state

--- src/App.js
+++ src/App.js
@@ -1,79 +1,96 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, Component} from 'react';
 import './App.css';
 import Navbar from './componentes/Navbar';
 import Characters  from './componentes/Character';
 import Buscador from './componentes/Buscador';
 import Pagination from './componentes/Pagination';
 
-function App() {
+useEffect(() => {
+  this.datosBusqueda();
+}, [])
 
-  var state = " ";
-  var pag = 1;
+class App extends Component {
 
-  function paginaAnterior() {
-    let pagina = pag;
-
-    if(pagina === 1 ) return null;
-
-    pagina -= 1;
-
-    pag = pagina;
-    consultaApi();
-  }
-
-  function paginaSiguiente() {
-    let pagina = pag;
-    console.log(pagina);
-
-
-    pagina = pagina + 1;
-
-    pag = pagina;
-
-    console.log(pag);
-    consultaApi();
+  state = {
+    termino: ' ',
+    imagenes : [],
+    pagina:''
   }
   
-  function datosBusqueda(termino) {
-    state = termino;
-    console.log(state);
-    consultaApi();
+
+  paginaAnterior = () =>{
+    let pagina = this.state.pagina;
+    if(pagina === 1) return null;
+    pagina-=1;
+    this.setState({pagina},()=>{
+      this.consultarApi();
+    });
+  }
+  paginaSiguiente = () =>{
+    let pagina = this.state.pagina;
+    pagina+=1;
+    this.setState({pagina}, ()=>{
+      this.consultarApi();
+    });
   }
 
-  function consultaApi() {
-    const urlB = 'https://pixabay.com/api/?key=25839045-809b64a8b48695c9127b61dde&q='+state+'&per_page=12&page='+pag;
-    console.log(urlB)
-    fetchCharacters(urlB);
-
-  }
-
-  const [characters, setCharacters] = useState([]);
-
-  const api = "https://pixabay.com/api/?key=25839045-809b64a8b48695c9127b61dde&q=&per_page=12"
-
-  const fetchCharacters = (url) => {
+  
+  consultarApi = () =>{
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=25838828-7a5ac27d64c7e99873e9e5729&q=${termino}&per_page=30&page=${pagina}`;
+    //console.log(url);
+     
     fetch(url)
-      .then(response => response.json())
-      .then(data => setCharacters(data.hits))
-      .catch(error => console.log(error))
+    .then(respuesta => respuesta.json())
+    .then(resultado => this.setState({imagenes : resultado.hits}))
   }
 
-  useEffect(() => {
-    fetchCharacters(api);
-  }, [])
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino : termino,
+      pagina : 1
+    }, ()=>{
+      this.consultarApi();
+    })
+  }
 
 
-  return (
-    <>
-      <Navbar brand="React pixebay"></Navbar>
-      <div className="container">
-        <Buscador datosBusqueda={datosBusqueda}></Buscador>
-        <Characters characters={characters}></Characters>
-        <Pagination paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior}></Pagination>
-      </div>
-    </>
 
-  );
+  // function consultaApi() {
+  //   const urlB = 'https://pixabay.com/api/?key=25839045-809b64a8b48695c9127b61dde&q='+state+'&per_page=12&page='+pag;
+  //   console.log(urlB)
+  //   fetchCharacters(urlB);
+
+  // }
+
+  // const [characters, setCharacters] = useState([]);
+
+  // const api = "https://pixabay.com/api/?key=25839045-809b64a8b48695c9127b61dde&q=&per_page=12"
+
+  // const fetchCharacters = (url) => {
+  //   fetch(url)
+  //     .then(response => response.json())
+  //     .then(data => setCharacters(data.hits))
+  //     .catch(error => console.log(error))
+  // }
+
+
+  render() {
+
+    return (
+      <>
+        <Navbar brand="React pixebay"></Navbar>
+        <div className="container">
+          <Buscador datosBusqueda={datosBusqueda}></Buscador>
+          <Characters characters={characters}></Characters>
+          <Pagination paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior}></Pagination>
+        </div>
+      </>
+
+    );
+  }
 }
 
 export default App;
